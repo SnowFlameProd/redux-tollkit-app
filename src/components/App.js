50e636b47e525784da9682324c59997f740bdb36
SnowFlame <genylep@mail.ru>
2024-05-23 18:30:27 +0300

    created store, added buttons for increment/decrement

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount } from '../slices/counterSlice';
+
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+      <div>
+        <div>
+          <button
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+          >
+            Прибавить
+          </button>
+          <span>{count}</span>
+          <button
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+          >
+            Отнять
+          </button>
+          <br />
+          <button onClick={() => dispatch(incrementByAmount(42))}>Прибавить 42</button>
+        </div>
+      </div>
+  );
+}
+
+export default App;
